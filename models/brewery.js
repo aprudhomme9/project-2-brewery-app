e42ddc293e0569ae0ef2001907670e11c2ba9672
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-19 14:32:20 -0500

    Added brewery model

---
+++ models/brewery.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Brewery = require('./brewery')
+
+const brewerySchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	location: String, //Google Places API
+	map: //<--- API?
+	visited: Boolean,
+	dogfriendly: Boolean,
+	bikefriendly: Boolean,
+	atmosphere: String,
+	price: String, //Google Places API
+	website: String, //Google Places API
+	reviews: [String], //Public
+	notes: [String] //For user
+});
+
+module.exports = mongoose.model('Brewery', brewerySchema);
