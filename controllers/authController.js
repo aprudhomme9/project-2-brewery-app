72042cabda6dd32f55e40ec2d6ee837c653ed62a
Ashley <ashley@Jamess-MacBook-Air.local>
2018-10-23 11:27:48 -0500

    modified routes for authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,32 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-//Login Render
-
+//Register Render
+router.get('/register', async (req, res, next) => {
+  res.render('auth/register.ejs', {
+    message: req.session.message
+  });
+});
 
 //Register Post
 
+
+//Login Render
+router.get('/login', async (req, res, next) => {
+  res.render('/', {
+    message: req.session.message
+  });
+});
 //Login Post
 
+
 //Logout Get
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    res.redirect('/');
+  });
+});
+
 
 
 
